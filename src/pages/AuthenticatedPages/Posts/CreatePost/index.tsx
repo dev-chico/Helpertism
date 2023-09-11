4621364b5d6a2a39b718c859cb563674825d3470
Luis Antonio de Souza Silva <dev.luis2003@gmail.com>
2023-09-11 20:14:22 -0300

    create structure of create post page

--- src/pages/AuthenticatedPages/Posts/CreatePost/index.tsx
+++ src/pages/AuthenticatedPages/Posts/CreatePost/index.tsx
@@ -1,9 +1,33 @@
+import { Button } from "../../../../components";
+import { FormGroup } from "../../../../components/FormGroup";
+import defaultImage from "../../../../assets/imgs/defaultImg.png";
 import styles from "./createPost.module.css";
 
 export function CreatePost() {
   return (
     <div className={styles.container}>
-      <h1>CreatePost</h1>
+      <header>
+        <h1>Novo post</h1>
+      </header>
+
+      <section className={styles.uploadImageContainer}>
+        <img
+          src={defaultImage}
+          alt="Imagem de um triângulo, um quadrado e um círculo cinzas"
+        />
+        <Button small bgColor="gray">
+          Escolher arquivo
+        </Button>
+      </section>
+
+      <form>
+        <FormGroup label="Digite seu título" placeholder="Título..." />
+
+        <textarea
+          className={styles.postContent}
+          placeholder="Era uma vez..."
+        ></textarea>
+      </form>
     </div>
   );
 }

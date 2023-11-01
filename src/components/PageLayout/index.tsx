54a0be11e57d27ca7b29126b7666a7af544ae69a
Luis Antonio de Souza Silva <dev.luis2003@gmail.com>
2023-11-01 18:24:21 -0300

    feat: create button of scroll to top

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Header } from "../Header";
 import { Menu } from "../Menu";
+import { FaArrowUp } from "react-icons/fa";
 import styles from "./pageLayout.module.css";
 
 interface IPageLayoutProps {
@@ -9,6 +10,21 @@ interface IPageLayoutProps {
 
 export function PageLayout({ children }: IPageLayoutProps) {
   const viewSize = window.innerWidth;
+  const [pageYPosition, setPageYPosition] = useState(0);
+
+  function getPageYAfterScroll() {
+    setPageYPosition(window.scrollY);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", getPageYAfterScroll);
+    return () => window.removeEventListener("scroll", getPageYAfterScroll);
+  }, []);
+
+  function backToTop() {
+    window.scrollTo(0, 0);
+  }
+
   return (
     <div className={`${styles.container} ${viewSize < 900 && styles.mobile}`}>
       <section className={styles.header}>
@@ -22,6 +38,16 @@ export function PageLayout({ children }: IPageLayoutProps) {
       )}
 
       <main className={styles.content}>{children}</main>
+
+      <button
+        type="button"
+        className={`${styles.backToTop} ${
+          pageYPosition > 400 && styles.visible
+        }`}
+        onClick={backToTop}
+      >
+        <FaArrowUp size={28} color="#FFF" />
+      </button>
     </div>
   );
 }
